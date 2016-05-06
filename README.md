e28f42a65ed476a33765cbe636b0ae837a2c2791
rbrto <robertoesparza3@gmail.com>
2016-05-05 23:30:30 -0300

    ADD: archivos iniciales

---
+++ README.md
@@ -0,0 +1,18 @@
+# Postgresql-Docker-Rails
+
+Docker image for PostgreSQL
+
+**Get it running:**
+
+``$ docker-compose up -d ``
+
+**Testing the running PostgreSQL**
+
+To test the running container we can use any client, even the commandline one:
+
+
+``psql -h localhost -p 5432 -U pguser -W pgdb``
+
+When you are prompted for password, type: pguser
+
+Enjoy !!!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '5f02a30d063cae6e08dd7e54888408abdb562c55aeed271ab0828cb9e7138c0dde02a990893604f346f19c760e8a9c55376c5baee30425078e35282b1f77e9a7'
